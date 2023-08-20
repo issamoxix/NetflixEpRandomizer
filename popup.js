444c7d794631f5670b36f4407bf4cb54b86fae80
Issam Haidaoui <issamhaidaoui@gmail.com>
2023-08-20 22:54:08 +0200

    modified UI

--- popup.js
+++ popup.js
@@ -1,5 +1,6 @@
 document.getElementById('sendMessage').addEventListener('click', function () {
     document.getElementById("loadingGif").classList.remove("hide");
+    document.getElementById("nloading").classList.add("hide");
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.sendMessage(tabs[0].id, { message: "hallo" });
     });
